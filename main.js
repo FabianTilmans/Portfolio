da261285e815a5b837759306077778091d4e7a92
FabianTil <fabian.tilmans@gmx.de>
2025-01-12 12:50:35 +0100

    Added theme-switching

---
+++ main.js
@@ -0,0 +1,15 @@
+(({ buttonSelector = '.theme-switcher', documentClass = 'light-theme' } = {}) => {
+  const button = document.querySelector(buttonSelector);
+  console.log(button);
+  if (!button || !window.CSS || !CSS.supports('top', 'var(--v)')) return;
+
+  button.hidden = false;
+
+  button.addEventListener('click', () => {
+    console.log("test");;
+    const newTheme = localStorage.getItem('theme') ? '' : documentClass;
+
+    localStorage.setItem('theme', newTheme);
+    document.documentElement.className = newTheme;
+  });
+})();
